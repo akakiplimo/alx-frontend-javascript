b6556dc9c13936facaf3432eba489841e3ea62f6
akakiplimo <akakiplimo@gmail.com>
2022-12-21 16:21:18 -0800

    syntax fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup () {
+export default function handleProfileSignup() {
   let firstName;
   let lastName;
   let body;
 
-  const photo = uploadPhoto();
-  const user = createUser();
+  const p = uploadPhoto();
+  const u = createUser();
 
-  return Promise.all([photo, user]).then((data) => {
+  return Promise.all([p, u]).then((data) => {
     body = data[0].body;
     firstName = data[1].firstName;
     lastName = data[1].lastName;
