cb47c67e62502c675adb109eca05a167b994104a
akakiplimo <akakiplimo@gmail.com>
2022-12-21 17:50:01 -0800

    fix: syntax error

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,8 +2,8 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   try {
-    return {photo: await uploadPhoto(), user: await createUser()}
+    return ({ photo: await uploadPhoto(), user: await createUser() });
   } catch (e) {
-    return {photo: null, user: null}
+    return ({ photo: null, user: null });
   }
 }
