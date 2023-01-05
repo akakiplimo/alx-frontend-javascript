c4bc38af44aa8945bf117daef9c3a802902a85d9
akakiplimo <akakiplimo@gmail.com>
2023-01-04 16:20:19 -0800

    feat: Printing teachers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,32 @@ interface Teacher {
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+const teacher1: Teacher = {
+  firstName: "Bi",
+  lastName: "Asira",
+  fullTimeEmployee: true,
+  yearsOfExperience: 5,
+  location: "Nairobi",
+  contract: true,
+};
+
+//console.log(teacher1);
+
+const director1: Directors = {
+  firstName: 'Mike',
+  lastName: 'Kip',
+  location: 'Keiyo',
+  fullTimeEmployee: true,
+  numberOfReports: 15,
+};
+
+//console.log(director1);
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+export const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => `${firstName[0]}. ${lastName}`;
+
+//console.log(printTeacher);
