46bd03950980b720005602226a59e4c37de4d917
akakiplimo <akakiplimo@gmail.com>
2023-01-06 05:03:13 -0800

    fix: space before closing curly

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,3 +1,3 @@
 export default function updateStudentGradeByCity(studentsList, city, newGrades) {
-  return studentsList.filter((std) => std.location === city).map((std) => ({ ...std, grade: newGrades.filter((g) => g.studentId === std.id).map((g) => g.grade)[0] || 'N/A'}));
+  return studentsList.filter((std) => std.location === city).map((std) => ({ ...std, grade: newGrades.filter((g) => g.studentId === std.id).map((g) => g.grade)[0] || 'N/A' }));
 }
