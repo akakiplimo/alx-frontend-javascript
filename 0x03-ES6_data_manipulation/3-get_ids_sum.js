6415a71587da8a47d02658fd3208e4fb7c2a1f3c
akakiplimo <akakiplimo@gmail.com>
2023-01-06 04:41:13 -0800

    fix: variable names

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
-export default function getStudentIdsSum(studentLst) {
-  return studentLst.reduce((sum, current) => sum + current.id, 0);
+export default function getStudentIdsSum(studentsLst) {
+  return studentsLst.reduce((sum, current) => sum + current.id, 0);
 }
